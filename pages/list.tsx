67f39c34a983e7ee1657dbe8503b326eb3d86f3e
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-11-02 22:42:39 +0100

    clean code - bgContainer in _app

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import BgImage from '../public/assets/Abstract-Gradient-5.png'
-import styled from 'styled-components'
 import Head from 'next/head'
 
 export default function list() {
@@ -11,17 +8,8 @@ export default function list() {
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<title>HRNET - Employees</title>
 		</Head>
-		<BgContainer>
-			<Image src={BgImage} alt="background" quality={100} fill placeholder="blur" />
-		</BgContainer>
+		
 		<h1>Employees</h1>
 	</>
 	)
 }
-
-const BgContainer = styled.div`
-	z-index: -1;
-	position: fixed;
-	width: 100vw;
-	height: 100vh;
-`
