1d234767933f673bd9e9772b3d4afa93a4a3c6b1
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-11-07 12:05:10 +0100

    try to fix img background path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-
-const isProd = process.env.NODE_ENV === "production"
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
 	reactStrictMode: true,
@@ -11,17 +10,19 @@ const nextConfig = {
 		styledComponents: true,
 	},
 	/**
-   * Tell Next.js where the `public` folder is.
-   */
-	 assetPrefix: isProd ? "/hrnet/" : "",
-	 /**
-	  * Disable server-based image optimization.
-	  *
-	  * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
-	  */
-	 images: {
-	   unoptimized: true,
-	 },
+	 * Tell Next.js where the `public` folder is.
+	 */
+	assetPrefix: isProd ? '/hrnet/' : '',
+	/**
+	 * Disable server-based image optimization.
+	 *
+	 * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+	 */
+	basePath: '/assets',
+	images: {
+		unoptimized: true,
+	},
 }
 
 module.exports = nextConfig
+
