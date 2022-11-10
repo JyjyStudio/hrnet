3df7b858e86f09e528ea2f16b03cac07e1611f34
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-11-10 16:54:13 +0100

    clean code + dispatch action on submit

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,34 +1,35 @@
-import { useState } from "react";
-import DatePicker from "./DatePicker";
-import DepartmentDropdown from "./DepartmentDropdown";
-import StateDropdown from "./StateDropdown";
-import Input from "./Input";
-import styled from "styled-components";
-import Modal from "../Modal/Modal";
+import { useState } from 'react'
+import DatePicker from './DatePicker'
+import DepartmentDropdown from './DepartmentDropdown'
+import StateDropdown from './StateDropdown'
+import Input from './Input'
+import styled from 'styled-components'
+import Modal from '../Modal/Modal'
+import { useTsDispatch } from '../../utils/redux/hooks'
+import {
+	addEmployee,
+	Employee,
+} from '../../utils/features/employees/EmployeesSlice'
 
 export default function Form() {
-
-
-	
 	// local state to store every input value
 	const [modal, setModal] = useState(false)
-	const [firstname, setFirstname] = useState("")
-	const [lastname, setLastname] = useState("")
-	const [dateOfBirth, setDateOfBirth] = useState(new Date())
-	const [startDate, setStartDate] = useState(new Date())
-	const [street, setStreet] = useState("")
-	const [city, setCity] = useState("")
-	const [state, setState] = useState("")
-	const [zipCode, setZipCode] = useState("")
-	const [department, setDepartment] = useState("")
+	const [firstname, setFirstname] = useState('')
+	const [lastname, setLastname] = useState('')
+	const [dateOfBirth, setDateOfBirth] = useState(new Date().toLocaleDateString())
+	const [startDate, setStartDate] = useState(new Date().toLocaleDateString())
+	const [street, setStreet] = useState('')
+	const [city, setCity] = useState('')
+	const [state, setState] = useState('')
+	const [zipCode, setZipCode] = useState('')
+	const [department, setDepartment] = useState('')
 
-	// const dispatch = useTsDispatch()
+	const dispatch = useTsDispatch()
 
 	const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
 		event.preventDefault()
-		// dispatch(addEmployee())
 		setModal(true)
-		const newEmployee = {
+		const newEmployee: Employee = {
 			firstname,
 			lastname,
 			dateOfBirth,
@@ -37,49 +38,96 @@ export default function Form() {
 			city,
 			state,
 			zipCode,
-			department
+			department,
 		}
+		dispatch(addEmployee(newEmployee))
 
-		console.log(newEmployee);
-		
+		console.log(newEmployee)
 	}
-  	return (
+
+	return (
 		<>
-		<StyledForm onSubmit={handleSubmit}>
-			<h2 style={{textAlign: 'center'}}>Create Employee</h2>
-			<Input type='text' name='first-name' value={firstname} setFunction={setFirstname} required/>
-			<Input type='text' name='last-name' value={lastname} setFunction={setLastname} required/>
-			<DatePicker value={dateOfBirth} setFunction={setDateOfBirth} label='date-of-birth' maxDate={new Date()} required />
-			<DatePicker value={startDate} setFunction={setStartDate} label='start-date' required noWeekends/>
-			<Fieldset>
-				<Input type='text' name='street' value={street} setFunction={setStreet} required/>
-				<Legend>Address</Legend>
-				<Input type='text' name='city' value={city} setFunction={setCity} required/>
-				<StateDropdown setFunction={setState} />
-				<Input type='number' name='zip-code' value={zipCode} setFunction={setZipCode} min={0} required/>
-			</Fieldset>
-			
-			<DepartmentDropdown setFunction={setDepartment} />
-			<SubmitBtn type="submit">Save</SubmitBtn>
-		</StyledForm>
-		
-		{modal && <Modal setModal={setModal}>Employee Created Successfully !</Modal>}
-		</>	
-  )
+			<StyledForm onSubmit={handleSubmit}>
+				<h2 style={{ textAlign: 'center' }}>Create Employee</h2>
+				<Input
+					type="text"
+					name="first-name"
+					value={firstname}
+					setFunction={setFirstname}
+					required
+				/>
+				<Input
+					type="text"
+					name="last-name"
+					value={lastname}
+					setFunction={setLastname}
+					required
+				/>
+				<DatePicker
+					value={dateOfBirth}
+					setFunction={setDateOfBirth}
+					label="date-of-birth"
+					maxDate={new Date()}
+					required
+				/>
+				<DatePicker
+					value={startDate}
+					setFunction={setStartDate}
+					label="start-date"
+					required
+					noWeekends
+				/>
+				<Fieldset>
+					<Input
+						type="text"
+						name="street"
+						value={street}
+						setFunction={setStreet}
+						required
+					/>
+					<Legend>Address</Legend>
+					<Input
+						type="text"
+						name="city"
+						value={city}
+						setFunction={setCity}
+						required
+					/>
+					<StateDropdown setFunction={setState} />
+					<Input
+						type="number"
+						name="zip-code"
+						value={zipCode}
+						setFunction={setZipCode}
+						min={0}
+						required
+					/>
+				</Fieldset>
+
+				<DepartmentDropdown setFunction={setDepartment} />
+				<SubmitBtn type="submit">Save</SubmitBtn>
+			</StyledForm>
+			{modal && (
+				<Modal setModal={setModal}>
+					Employee Created Successfully !
+				</Modal>
+			)}
+		</>
+	)
 }
 
 const StyledForm = styled.form`
 	display: flex;
 	flex-direction: column;
 	margin: 1rem 0;
-	background: rgba( 255, 255, 255, 0.45 );
-    box-shadow: 0 8px 32px 0 rgb(31 38 135 / 37%);
-    backdrop-filter: blur( 1.5px );
-    -webkit-backdrop-filter: blur( 1.5px );
-    border: 1px solid rgba( 255, 255, 255, 0.18 );
-    padding: 5px 1rem 10px;
-    width: 50%;
-    max-width: 400px;
+	background: rgba(255, 255, 255, 0.45);
+	box-shadow: 0 8px 32px 0 rgb(31 38 135 / 37%);
+	backdrop-filter: blur(1.5px);
+	-webkit-backdrop-filter: blur(1.5px);
+	border: 1px solid rgba(255, 255, 255, 0.18);
+	padding: 5px 1rem 10px;
+	width: 50%;
+	max-width: 400px;
 	@media screen and (max-width: 700px) {
 		width: 90%;
 		margin: 2rem auto 1rem;
@@ -93,7 +141,7 @@ const Fieldset = styled.fieldset`
 `
 const Legend = styled.legend`
 	padding: 0 5px;
-    line-height: 1rem;
+	line-height: 1rem;
 `
 const SubmitBtn = styled.button`
 	margin-top: 1rem;
