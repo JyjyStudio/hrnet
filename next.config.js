03e8e1bc4bb492bd60b81759b6326ef78c9f9629
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-11-13 19:02:07 +0100

    test

--- next.config.js
+++ next.config.js
@@ -6,10 +6,7 @@ const nextConfig = {
 	compiler: {
 		// Enables the styled-components SWC transform
 		styledComponents: true,
-	},
-	images: {
-		unoptimized: true,
-	},
+	}
 }
 
 module.exports = nextConfig
