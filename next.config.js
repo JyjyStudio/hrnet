0edf3677638c75e1ff02165af0daff746caa7e70
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-11-13 19:07:36 +0100

    test image optimization

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
 		// Enables the styled-components SWC transform
 		styledComponents: true,
 	},
+	images: {
+		loader: 'custom',
+		unoptimized: true
+	},
 	experimental: {
 		images: {
 			unoptimized: true
