ff945189c75b1c319a115dafb7fee6198784f051
“jyhad” <jyhad.benyoussef@gmail.com>
2022-11-17 13:56:27 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,6 @@ export default function HomePage(): JSX.Element {
 				<H1>HRnet</H1>
 				<StyledLink href="employees">View Current Employees</StyledLink>
 			</Headings>
-				<h2>test</h2>
 			<Form />
 		</Container>
 
