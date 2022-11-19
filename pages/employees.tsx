28640e0a43377b51c5c505b1312df23a6d27f779
jyhad <jyhad.benyoussef@gmail.com>
2022-11-19 10:35:17 +0100

    link to homepage added

--- pages/employees.tsx
+++ pages/employees.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import EmployeesTable from '../components/Table/Table'
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace'
+import Link from 'next/link'
+import styled from "styled-components"
+
 export default function list() {
 	return (
 		<>
@@ -8,8 +12,18 @@ export default function list() {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>HRNET - Employees</title>
 			</Head>
-
+			<StyledTitle>
+				<Link href="/">
+					<KeyboardBackspaceIcon /> Add a new employee
+				</Link>
+			</StyledTitle>
 			<EmployeesTable />
 		</>
 	)
 }
+
+const StyledTitle = styled.h2`
+	position: absolute;
+	top: 20%;
+	left: 5%;
+` 
