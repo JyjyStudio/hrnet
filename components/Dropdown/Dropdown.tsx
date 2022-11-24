23c0d46febd25b72346383b1ef9b0c2a33c54957
jyhad <jyhad.benyoussef@gmail.com>
2022-11-24 19:29:47 +0100

    fix: dropdown used by react-hook-form

--- components/Dropdown/Dropdown.tsx
+++ components/Dropdown/Dropdown.tsx
@@ -2,25 +2,21 @@ import { Dropdown } from 'react-dropdown-now';
 import styles from './Dropdown.module.css'
 import 'react-dropdown-now/style.css';
 
-export default function PersonnalDropdown({options, setFunction, placeholder}:Props) {
+export default function PersonnalDropdown({options, onChange, placeholder}:Props) {
 
 	return (
-		<Dropdown 
+		<Dropdown
 			baseClassName="rdn"
 			className={styles.styled_Dropdown}
 			placeholder={placeholder}
 			options={options}
-			onChange={(value) => {
-				console.log('change!', value)
-				setFunction(value.value)
-			}}
-			// onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
+			onChange={onChange}
 		/>
 	)
 }
 
 interface Props {
 	options: Array<{id: string, value:string, view: any}> | Array<string>
-	setFunction: Function
 	placeholder: string
+	onChange: any
 }
