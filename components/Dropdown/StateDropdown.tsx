14e7d65dbdedda12d7b9a528e8f692871944b970
jyhad <jyhad.benyoussef@gmail.com>
2022-11-29 13:35:06 +0100

    change architecture folders

---
+++ components/Dropdown/StateDropdown.tsx
@@ -0,0 +1,78 @@
+import CustomizedMenus from './Dropdown'
+
+export default function StateDropdown({ onChange }: Props) {
+	const states: Array<string> = [
+		'Alabama',
+		'Alaska',
+		'American Samoa',
+		'Arizona',
+		'Arkansas',
+		'California',
+		'Colorado',
+		'Connecticut',
+		'Delaware',
+		'District of Columbia',
+		'Federated States of Micronesia',
+		'Florida',
+		'Georgia',
+		'Guam',
+		'Hawaii',
+		'Idaho',
+		'Illinois',
+		'Indiana',
+		'Iowa',
+		'Kansas',
+		'Kentucky',
+		'Louisiana',
+		'Maine',
+		'Marshall Islands',
+		'Maryland',
+		'Massachusetts',
+		'Michigan',
+		'Minnesota',
+		'Mississippi',
+		'Missouri',
+		'Montana',
+		'Nebraska',
+		'Nevada',
+		'New Hampshire',
+		'New Jersey',
+		'New Mexico',
+		'New York',
+		'North Carolina',
+		'North Dakota',
+		'Northern Mariana Islands',
+		'Ohio',
+		'Oklahoma',
+		'Oregon',
+		'Palau',
+		'Pennsylvania',
+		'Puerto Rico',
+		'Rhode Island',
+		'South Carolina',
+		'South Dakota',
+		'Tennessee',
+		'Texas',
+		'Utah',
+		'Vermont',
+		'Virgin Island',
+		'Virginia',
+		'Washington',
+		'West Virginia',
+		'Wisconsin',
+		'Wyoming',
+	]
+
+	return (
+		<CustomizedMenus
+			options={states}
+			onChange={onChange}
+			defaultPlaceholder="Select a state"
+			scrollable
+		/>
+	)
+}
+
+interface Props {
+	onChange: Function
+}
--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -2,9 +2,9 @@ import { useForm, SubmitHandler } from 'react-hook-form'
 import styles from './Form.module.css'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { validationSchema } from './validationSchema'
-import DatePicker from './DatePicker'
-import StateDropdown from './StateDropdown'
-import DepartmentDropdown from './DepartmentDropdown'
+import DatePicker from '../Datepicker/DatePicker'
+import StateDropdown from '../Dropdown/StateDropdown'
+import DepartmentDropdown from '../Dropdown/DepartmentDropdown'
 import { useState } from 'react'
 import { useTsDispatch } from '../../utils/redux/hooks'
 import { addEmployee } from '../../utils/features/employees/EmployeesSlice'
@@ -23,11 +23,17 @@ type Inputs = {
 }
 
 export default function Form() {
-
-	const { register, setValue, handleSubmit, watch, formState: { errors }, reset } = useForm<Inputs>({resolver: yupResolver(validationSchema),})
+	const {
+		register,
+		setValue,
+		handleSubmit,
+		watch,
+		formState: { errors },
+		reset,
+	} = useForm<Inputs>({ resolver: yupResolver(validationSchema) })
 	const [modal, setModal] = useState(false)
 	const dispatch = useTsDispatch()
-	
+
 	const onSubmit: SubmitHandler<Inputs> = (newEmployee) => {
 		setModal(true)
 		console.log('newEmployee:', newEmployee)
@@ -35,129 +41,174 @@ export default function Form() {
 		reset()
 	}
 	// console.log('dateOfBirth:', watch('dateOfBirth')) // watch input value by passing the name of it
-	
+
 	return (
-	<>
-		{/* "handleSubmit" will validate inputs before invoking "onSubmit" */}
-		<form className={styles.employee_form} onSubmit={handleSubmit(onSubmit)}>
-			
-			{/* Firstname */}
-			<div className={styles.input_container}>
-				<label className={styles.label} htmlFor="firstname">First Name</label>
-				<small className={styles.error_msg} role="alert">{errors.firstname?.message}</small>
-				<input
-					id='firstname'
-					className={errors.firstname && styles.input_error}
-					aria-invalid={errors.firstname ? "true" : "false"}
-					{...register('firstname', {
-						pattern: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g,
-						required: true
-					})}
-				/>
-			</div>
-			{/* Lastname */}
-			<div className={styles.input_container}>
-				<label className={styles.label} htmlFor="lastname">Last Name</label>
-				<small className={styles.error_msg} role="alert">{errors.lastname?.message}</small>
-				<input
-					id='lastname'
-					className={errors.lastname && styles.input_error}
-					aria-invalid={errors.lastname ? "true" : "false"}
-					{...register('lastname', { required: true })} 
-				/>
-			</div>
-			{/* Date of birth */}
-			<div className={styles.input_container}>
-				<label className={styles.label} htmlFor="dateOfBirth">Date Of Birth</label>
-				<small className={styles.error_msg} role="alert">{errors.dateOfBirth?.message}</small>
-				<DatePicker
-					error={errors.dateOfBirth}
-					aria-invalid={errors.dateOfBirth ? "true" : "false"}
-					maxDate={new Date()}
-					onChange={(val:string) => {
-						setValue("dateOfBirth", val)
-						register("dateOfBirth", { required: true })
-					}}
-				/>
-			</div>
-			{/* Start Date */}
-			<div className={styles.input_container}>
-				<label className={styles.label} htmlFor="startDate">Start Date</label>
-				<small className={styles.error_msg} role="alert">{errors.startDate?.message}</small>
-				<DatePicker
-					error={errors.startDate}
-					aria-invalid={errors.startDate ? "true" : "false"}
-					noWeekends
-					onChange={(val:string) => {
-						setValue("startDate", val)
-						register("startDate", { required: true })
-					}}
-				/>
-			</div>
-			<fieldset className={styles.employee_form__fieldset}>
-				<legend className={styles.employee_form__legend}>Adress</legend>
-				
-				{/* Street */}
+		<>
+			{/* "handleSubmit" will validate inputs before invoking "onSubmit" */}
+			<form
+				className={styles.employee_form}
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				{/* Firstname */}
 				<div className={styles.input_container}>
-					<label className={styles.label} htmlFor="street">Street</label>
-					<small className={styles.error_msg} role="alert">{errors.street?.message}</small>
+					<label className={styles.label} htmlFor="firstname">
+						First Name
+					</label>
+					<small className={styles.error_msg} role="alert">
+						{errors.firstname?.message}
+					</small>
 					<input
-						id='street'
-						className={errors.street && styles.input_error}
-						aria-invalid={errors.street ? "true" : "false"}
-						{...register('street', { required: true })} 
+						id="firstname"
+						className={errors.firstname && styles.input_error}
+						aria-invalid={errors.firstname ? 'true' : 'false'}
+						{...register('firstname', {
+							pattern:
+								/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g,
+							required: true,
+						})}
 					/>
 				</div>
-				{/* City */}
+				{/* Lastname */}
 				<div className={styles.input_container}>
-					<label className={styles.label} htmlFor="city">City</label>
-					<small className={styles.error_msg} role="alert">{errors.city?.message}</small>
+					<label className={styles.label} htmlFor="lastname">
+						Last Name
+					</label>
+					<small className={styles.error_msg} role="alert">
+						{errors.lastname?.message}
+					</small>
 					<input
-						id='city'
-						className={errors.city && styles.input_error}
-						aria-invalid={errors.city ? "true" : "false"}
-						{...register('city', { required: true })} 
+						id="lastname"
+						className={errors.lastname && styles.input_error}
+						aria-invalid={errors.lastname ? 'true' : 'false'}
+						{...register('lastname', { required: true })}
 					/>
 				</div>
-				{/* State Dropdown */}
+				{/* Date of birth */}
 				<div className={styles.input_container}>
-					<label className={styles.label} htmlFor="state">State</label>
-					<small className={styles.error_msg} role="alert">{errors.state?.message}</small>
-					<StateDropdown 
-						onChange={(value:string) => {
-							setValue("state", value)
-							register('state', { required: true })
+					<label className={styles.label} htmlFor="dateOfBirth">
+						Date Of Birth
+					</label>
+					<small className={styles.error_msg} role="alert">
+						{errors.dateOfBirth?.message}
+					</small>
+					<DatePicker
+						error={errors.dateOfBirth}
+						aria-invalid={errors.dateOfBirth ? 'true' : 'false'}
+						maxDate={new Date()}
+						onChange={(val: string) => {
+							setValue('dateOfBirth', val)
+							register('dateOfBirth', { required: true })
 						}}
 					/>
 				</div>
-				{/* zipCode */}
+				{/* Start Date */}
 				<div className={styles.input_container}>
-					<label className={styles.label} htmlFor="zipCode">Zip Code</label>
-					<small className={styles.error_msg} role="alert">{errors.zipCode?.message}</small>
-					<input
-						id='zipCode'
-						className={errors.zipCode && styles.input_error}
-						aria-invalid={errors.zipCode ? "true" : "false"}
-						{...register('zipCode', { required: true })} 
+					<label className={styles.label} htmlFor="startDate">
+						Start Date
+					</label>
+					<small className={styles.error_msg} role="alert">
+						{errors.startDate?.message}
+					</small>
+					<DatePicker
+						error={errors.startDate}
+						aria-invalid={errors.startDate ? 'true' : 'false'}
+						noWeekends
+						onChange={(val: string) => {
+							setValue('startDate', val)
+							register('startDate', { required: true })
+						}}
 					/>
 				</div>
-			</fieldset>
-			{/* Department Dropdown */}
-			<div className={styles.input_container}>
-				<label className={styles.label} htmlFor="department">Department</label>
-				<small className={styles.error_msg} role="alert">{errors.department?.message}</small>
-				<DepartmentDropdown 
-					onChange={(value:string) => {
-						register("department", { required: true, min: 0 })
-						setValue("department", value)
-					}}
-				/>
-			</div>
-			<input className={styles.employee_form__submit} type="submit" />
-		</form>
+				<fieldset className={styles.employee_form__fieldset}>
+					<legend className={styles.employee_form__legend}>
+						Adress
+					</legend>
 
-		{/* modal */}
-		{ !!modal && <Modal setModal={setModal}>Employee Created Successfully !</Modal> }
-	</>
+					{/* Street */}
+					<div className={styles.input_container}>
+						<label className={styles.label} htmlFor="street">
+							Street
+						</label>
+						<small className={styles.error_msg} role="alert">
+							{errors.street?.message}
+						</small>
+						<input
+							id="street"
+							className={errors.street && styles.input_error}
+							aria-invalid={errors.street ? 'true' : 'false'}
+							{...register('street', { required: true })}
+						/>
+					</div>
+					{/* City */}
+					<div className={styles.input_container}>
+						<label className={styles.label} htmlFor="city">
+							City
+						</label>
+						<small className={styles.error_msg} role="alert">
+							{errors.city?.message}
+						</small>
+						<input
+							id="city"
+							className={errors.city && styles.input_error}
+							aria-invalid={errors.city ? 'true' : 'false'}
+							{...register('city', { required: true })}
+						/>
+					</div>
+					{/* State Dropdown */}
+					<div className={styles.input_container}>
+						<label className={styles.label} htmlFor="state">
+							State
+						</label>
+						<small className={styles.error_msg} role="alert">
+							{errors.state?.message}
+						</small>
+						<StateDropdown
+							onChange={(value: string) => {
+								setValue('state', value)
+								register('state', { required: true })
+							}}
+						/>
+					</div>
+					{/* zipCode */}
+					<div className={styles.input_container}>
+						<label className={styles.label} htmlFor="zipCode">
+							Zip Code
+						</label>
+						<small className={styles.error_msg} role="alert">
+							{errors.zipCode?.message}
+						</small>
+						<input
+							id="zipCode"
+							className={errors.zipCode && styles.input_error}
+							aria-invalid={errors.zipCode ? 'true' : 'false'}
+							{...register('zipCode', { required: true })}
+						/>
+					</div>
+				</fieldset>
+				{/* Department Dropdown */}
+				<div className={styles.input_container}>
+					<label className={styles.label} htmlFor="department">
+						Department
+					</label>
+					<small className={styles.error_msg} role="alert">
+						{errors.department?.message}
+					</small>
+					<DepartmentDropdown
+						onChange={(value: string) => {
+							register('department', { required: true, min: 0 })
+							setValue('department', value)
+						}}
+					/>
+				</div>
+				<input className={styles.employee_form__submit} type="submit" />
+			</form>
+
+			{/* modal */}
+			{!!modal && (
+				<Modal setModal={setModal}>
+					Employee Created Successfully !
+				</Modal>
+			)}
+		</>
 	)
 }
