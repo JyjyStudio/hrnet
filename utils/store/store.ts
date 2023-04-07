3254b44654e2c59d07e7ed6fc77e981c1bfc7a22
jyhad <jyhad.benyoussef@gmail.com>
2023-04-07 11:34:29 +0200

    createNoopStorage added

--- utils/store/store.ts
+++ utils/store/store.ts
@@ -1,8 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { combineReducers } from "redux"
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import employeesSlice from '../store/employees/EmployeesSlice'
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist"
+// import storage from 'redux-persist/lib/storage'
+import createWebStorage from "redux-persist/lib/storage/createWebStorage"
+import employeesSlice from "../store/employees/EmployeesSlice"
+
+const createNoopStorage = () => {
+	return {
+		getItem(_key: any) {
+			return Promise.resolve(null)
+		},
+		setItem(_key: any, value: any) {
+			return Promise.resolve(value)
+		},
+		removeItem(_key: any) {
+			return Promise.resolve()
+		},
+	}
+}
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage()
 
 const persistConfig = {
 	key: "root",
@@ -20,10 +45,10 @@ export const store = configureStore({
 	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
-		serializableCheck: {
-			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-		},
-	}),
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 })
 
 export const persistor = persistStore(store)
